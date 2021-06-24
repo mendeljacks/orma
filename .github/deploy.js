b1aea9d7f3e6876428a563e2ff77e55c53a94988
Mendel <mendeljacks@gmail.com>
2021-06-24 19:18:27 -0400

    orma

--- .github/deploy.js
+++ .github/deploy.js
@@ -1,9 +1,45 @@
 const npmPublish = require("@jsdevtools/npm-publish");
+const fs = require('fs')
+const spawn = require('cross-spawn')
 
 module.exports.deploy = async () => {
+
+    run_process(['npm', 'version', 'patch'])
+
+
     // Run npm-publish with options
     await npmPublish({
         package: "./package.json",
         token: process.env.NPM_TOKEN
     });
+}
+
+
+
+const run_process = async (arg_list) => {
+
+    return new Promise((resolve, reject) => {
+        console.log(`Executing: ${arg_list.join(' ')}`)
+        const child_process = spawn(
+            arg_list[0]
+            , arg_list.slice(0,-1),
+            { stdio: 'pipe' }
+        )
+
+        let output = []
+        let err = []
+        child_process.stdout.on('data', (data) => {
+            // console.log(data.toString())
+            output.push(data.toString())
+        });
+        child_process.stderr.on('data', (data) => {
+            // console.error(data.toString())
+            err.push(data.toString())
+        });
+        child_process.on('close', (code) => {
+            if (code !== 0) reject(err)
+            resolve(output)
+        })
+
+    })
 }
