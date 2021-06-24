d397efd2ccde4ce0771fd4b8c4c542988e9e97b0
Mendel <mendeljacks@gmail.com>
2021-06-24 16:56:28 -0400

    deploy

---
+++ .github/deploy.js
@@ -0,0 +1,9 @@
+const npmPublish = require("@jsdevtools/npm-publish");
+
+module.exports.deploy = async () => {
+    // Run npm-publish with options
+    await npmPublish({
+        package: "../package.json",
+        token: process.env.NPM_TOKEN
+    });
+}
