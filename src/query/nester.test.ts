be0c28802f6f1dde9a88ee5a24fd367862c01354
Mendel <mendeljacks@gmail.com>
2021-07-02 02:41:52 -0400

    object nest

--- src/query/nester.test.ts
+++ src/query/nester.test.ts
@@ -2,12 +2,12 @@ import { expect } from 'chai'
 import { describe, test } from 'mocha'
 import { nester } from '../helpers/nester'
 
-describe('nester', () => {
+describe.only('nester', () => {
     test('basic deep nesting', async () => {
         const data = [
-            [['vendors'], [{ id: 1 }, { id: 2 }]],
-            [['vendors', 0, 'products'], [{ id: 1, vendor_id: 2 }]],
-            [['vendors', 0, 'products', 0, 'images'], [{ product_id: 1 }, { product_id: 1 }]]
+            [['vendors', 0], [{ id: 1 }, { id: 2 }]],
+            [['vendors', 0, 'products', 0], [{ id: 1, vendor_id: 2 }]],
+            [['vendors', 0, 'products', 0, 'images', 0], [{ product_id: 1 }, { product_id: 1 }]]
         ]
 
         const edges = [
@@ -41,6 +41,46 @@ describe('nester', () => {
         expect(result).to.deep.equal(goal)
 
     })
+    test('object nesting', () => {
+        const data = [
+            [['vendors', 0], [{ id: 1 }, { id: 2 }]],
+            [['vendors', 0, 'products'], [{ id: 1, vendor_id: 2 }]],
+        ]
+
+        const edges = [
+            { from_entity: 'vendors', from_field: 'id', to_entity: 'products', to_field: 'vendor_id' },
+        ]
+
+
+        const goal = {
+            vendors: [{
+                id: 1
+            }, {
+                id: 2,
+                products: {
+                    id: 1,
+                    vendor_id: 2
+                }
+            }]
+        }
+
+        console.time('t')
+
+        let result = nester(data, edges)
+        // let result = {}
+        // for (let i = 0; i < 10000; i++) {
+        //     result = nester(data, edges)
+        // }
+        // await stop_profiler()
+        // console.timeEnd('t')
+
+
+        expect(result).to.deep.equal(goal)
+    })
+
+
+
+
     test.skip('breadth speed test', async () => {
         const vendors = new Array(100).fill(undefined).map((_, i) => ({
             id: Math.random() * 1000000000000000
@@ -94,7 +134,7 @@ describe('nester', () => {
 
         let result = {}
         // for (let i = 0; i < 10000; i++) {
-            result = nester(data, edges)
+        result = nester(data, edges)
         // }
         console.timeEnd('t')
 
@@ -102,40 +142,5 @@ describe('nester', () => {
         // expect(result).to.deep.equal(goal)
 
     })
-    test.skip('object nesting', () => {
-        // const data = [
-        //     [['vendors'], [{ id: 1 }, { id: 2 }]],
-        //     [['vendors', 'products'], [{ id: 1, vendor_id: 2 }]],
-        // ]
 
-        // const edges = [
-        //     { from_entity: 'vendors', from_field: 'id', to_entity: 'products', to_field: 'vendor_id' },
-        // ]
-
-
-        // const goal = {
-        //     vendors: [{
-        //         id: 1
-        //     }, {
-        //         id: 2,
-        //         products: {
-        //             id: 1,
-        //             vendor_id: 2
-        //         }
-        //     }]
-        // }
-
-        // console.time('t')
-
-        // let result = nester(data, edges)
-        // // let result = {}
-        // // for (let i = 0; i < 10000; i++) {
-        // //     result = nester(data, edges)
-        // // }
-        // // await stop_profiler()
-        // // console.timeEnd('t')
-
-
-        // expect(result).to.deep.equal(goal)
-    })
 })
