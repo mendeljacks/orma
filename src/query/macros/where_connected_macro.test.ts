6142decf0b3231d653140a58cc704b5c2dedb77e
Daniel Jacks <danielchaimjacks@gmail.com>
2022-04-26 11:48:27 -0400

    More work on where connected macro

--- src/query/macros/where_connected_macro.test.ts
+++ src/query/macros/where_connected_macro.test.ts
@@ -1,6 +1,11 @@
+import { expect } from 'chai'
 import { describe, test } from 'mocha'
+import { orma_schema } from '../../introspector/introspector'
 import { as_orma_schema } from '../query'
-import { apply_where_connected_macro } from './where_connected_macro'
+import {
+    apply_where_connected_macro,
+    get_upwards_connection_paths,
+} from './where_connected_macro'
 
 describe('where_connected_macro.ts', () => {
     const schema = as_orma_schema({
@@ -38,20 +43,103 @@ describe('where_connected_macro.ts', () => {
         },
     })
 
+    describe(get_upwards_connection_paths.name, () => {
+        test('generates nested paths', () => {
+            const schema: orma_schema = {
+                grandparents: {
+                    id: {},
+                },
+                parents: {
+                    id: {},
+                    grandparent_id: {
+                        references: {
+                            grandparents: {
+                                id: {},
+                            },
+                        },
+                    },
+                },
+                children: {
+                    id: {},
+                    parent_id: {
+                        references: {
+                            parents: {
+                                id: {},
+                            },
+                        },
+                    },
+                },
+            }
+
+            const connection_paths = get_upwards_connection_paths(schema)
+
+            expect(connection_paths.children).to.deep.equal([
+                [
+                    {
+                        from_entity: 'children',
+                        from_field: 'parent_id',
+                        to_entity: 'parents',
+                        to_field: 'id',
+                    },
+                    {
+                        from_entity: 'parents',
+                        from_field: 'parent_id',
+                        to_entity: 'grandparents',
+                        to_field: 'id',
+                    }
+                ],
+            ])
+        })
+    })
+
     describe(apply_where_connected_macro.name, () => {
         test('handles nested entities', () => {
             const query = {
                 $where_connected: {
                     grandparents: {
-                        id: [1, 2]
-                    }
+                        id: [1, 2],
+                    },
                 },
                 children: {
-                    id: true
-                }
+                    id: true,
+                },
             }
 
-            apply_where_connected_macro(query)
+            apply_where_connected_macro(query, {
+                children: [
+                    [
+                        {
+                            from_entity: 'children',
+                            from_field: 'parent_id',
+                            to_entity: 'parents',
+                            to_field: 'id',
+                        },
+                        {
+                            from_entity: 'parents',
+                            from_field: 'grandparent_id',
+                            to_entity: 'grandparents',
+                            to_field: 'id',
+                        },
+                    ],
+                ],
+            })
+
+            // @ts-ignore
+            expect(query.children.$where).to.deep.equal({
+                $in: [
+                    'parent_id',
+                    {
+                        $select: ['id'],
+                        $from: 'parents',
+                        $where: {
+                            // we dont need another layer of nesting since we are filtering against grandparents.id which
+                            // is guaranteed to match parents.grandparent_id since it has a foreign key
+                            $in: ['grandparent_id', [1, 2]],
+                        },
+                    },
+                ],
+            })
         })
+        test.skip('handles nesting for tables without connection paths')
     })
 })
--- src/query/macros/where_connected_macro.ts
+++ src/query/macros/where_connected_macro.ts
@@ -103,19 +103,20 @@
  * @module
  */
 
-import { dropRight, findLast, first, last } from 'lodash'
-import { deep_for_each } from '../../helpers/helpers'
-import { Edge } from '../../helpers/schema_helpers'
-import { OrmaQuery } from '../../types/query/query_types'
-import { OrmaSchema } from '../../types/schema_types'
+import { last } from '../../helpers/helpers'
+import {
+    Edge,
+    get_entity_names,
+    get_parent_edges,
+} from '../../helpers/schema_helpers'
+import { orma_schema } from '../../introspector/introspector'
 import { query_for_each } from '../query_helpers'
-import { deep_map } from '../utils/utils'
 
 /*
 This defines the concept of 'connected' for this macro. Each entity gets a list of paths that are considered connected.
 For each of these paths, where clauses will be generated.
  */
-export type OwnershipPaths = {
+export type ConnectionPaths = {
     [entity_name: string]: Edge[][]
 }
 
@@ -131,7 +132,49 @@ export type OwnershipPaths = {
  */
 export type Path = (string | number)[]
 
-export const apply_where_connected_macro = (query: Record<string, any>) => {
+export const get_upwards_connection_paths = (orma_schema: orma_schema) => {
+    const connection_paths = get_entity_names(orma_schema).reduce(
+        (acc, entity_name) => {
+
+            // start off with a path to every parent
+            const entity_paths = get_parent_edges(entity_name, orma_schema).map(
+                edge => [edge]
+            )
+
+            // every path before this index is done, in other words there are no more paths that we can get
+            // by appending some parent edge onto that path
+            let done_until_index = -1
+
+            // keep looping while there are still paths to process
+            while (done_until_index < entity_paths.length - 1) {
+                for (let i = done_until_index; i < entity_paths.length; i++) {
+                    // for each unprocessed path, generate new paths by appending all possible 
+                    // parent edges onto its end
+                    const entity_path = entity_paths[i]
+                    const parent_entity = last(entity_path).to_entity
+                    const new_paths = get_parent_edges(
+                        parent_entity,
+                        orma_schema
+                    ).map(new_edge => [...entity_path, new_edge])
+
+                    entity_paths.push(...new_paths)
+                }
+            }
+
+            acc[entity_name] = entity_paths
+
+            return acc
+        },
+        {} as ConnectionPaths
+    )
+
+    return connection_paths
+}
+
+export const apply_where_connected_macro = (
+    query: Record<string, any>,
+    connection_paths: ConnectionPaths
+) => {
     if (!query.$where_connected) {
         return
     }
@@ -141,159 +184,159 @@ export const apply_where_connected_macro = (query: Record<string, any>) => {
     })
 }
 
-/**
- * Recursively adds ownership clauses to the given prisma query, as discussed in the module comment.
- * @param root_table Prisma root table, for example prisma.users has root table 'users'
- * @param ownership_paths ownership paths as discussed here {@link OwnershipPaths}
- * @param owner_column the column in the owner table which we are checking ownership against. For example, 'username'
- * @param owner_values the owner values we want to restrict results to. For example 'john@gmail.com'
- * @returns A copy of the prisma query with ownership clauses
- */
-export const add_query_ownership_clauses = (
-    root_table: string,
-    ownership_paths: OwnershipPaths,
-    root_ownership_paths: OwnershipPaths,
-    owner_column: string,
-    owner_values: any[],
-    prisma_query: Record<string, unknown>
-) => {
-    const mapped_prisma_query = deep_map(prisma_query, (value, path) => {
-        const table_name = last(path)?.toString()
-        // deep map will be called on keys that are not tables, for example 'select' or 'where'. For these keys, do nothing
-        if (!table_name || !is_table_name(table_name, ownership_paths)) {
-            return value
-        }
+// /**
+//  * Recursively adds ownership clauses to the given prisma query, as discussed in the module comment.
+//  * @param root_table Prisma root table, for example prisma.users has root table 'users'
+//  * @param ownership_paths ownership paths as discussed here {@link ConnectionPaths}
+//  * @param owner_column the column in the owner table which we are checking ownership against. For example, 'username'
+//  * @param owner_values the owner values we want to restrict results to. For example 'john@gmail.com'
+//  * @returns A copy of the prisma query with ownership clauses
+//  */
+// export const add_query_ownership_clauses = (
+//     root_table: string,
+//     ownership_paths: ConnectionPaths,
+//     root_ownership_paths: ConnectionPaths,
+//     owner_column: string,
+//     owner_values: any[],
+//     prisma_query: Record<string, unknown>
+// ) => {
+//     const mapped_prisma_query = deep_map(prisma_query, (value, path) => {
+//         const table_name = last(path)?.toString()
+//         // deep map will be called on keys that are not tables, for example 'select' or 'where'. For these keys, do nothing
+//         if (!table_name || !is_table_name(table_name, ownership_paths)) {
+//             return value
+//         }
 
-        const ownership_path = get_ownership_path(ownership_paths, table_name)
+//         const ownership_path = get_ownership_path(ownership_paths, table_name)
 
-        // This is basically to hack around the fact that prisma syntax doesnt allow
-        // where clauses on nested parent tables
-        if (ownership_path === null) {
-            return value
-        }
+//         // This is basically to hack around the fact that prisma syntax doesnt allow
+//         // where clauses on nested parent tables
+//         if (ownership_path === null) {
+//             return value
+//         }
 
-        const parent_table = first(ownership_path)
-        const higher_table =
-            findLast(
-                dropRight(path, 1), // exclude current table, otherwise higher_table will always equal the current table
-                el => is_table_name(el, ownership_paths) // only look for keys in the path that are table names. E.g. exclude 'where' or 'select'
-            ) ?? root_table // if nothing is found, that means we are the child of the root table, so use that as the higher table
+//         const parent_table = first(ownership_path)
+//         const higher_table =
+//             findLast(
+//                 dropRight(path, 1), // exclude current table, otherwise higher_table will always equal the current table
+//                 el => is_table_name(el, ownership_paths) // only look for keys in the path that are table names. E.g. exclude 'where' or 'select'
+//             ) ?? root_table // if nothing is found, that means we are the child of the root table, so use that as the higher table
 
-        // (optimization) if the higher table is the parent table, we dont need to do extra filtering. This is because prisma will already filter
-        // this to be a child of the higher table, so we only need to put the extra where clause on the higher table
-        if (parent_table === higher_table) {
-            return value
-        }
+//         // (optimization) if the higher table is the parent table, we dont need to do extra filtering. This is because prisma will already filter
+//         // this to be a child of the higher table, so we only need to put the extra where clause on the higher table
+//         if (parent_table === higher_table) {
+//             return value
+//         }
 
-        const ownership_where = generate_ownership_where(
-            ownership_path,
-            owner_column,
-            owner_values
-        )
+//         const ownership_where = generate_ownership_where(
+//             ownership_path,
+//             owner_column,
+//             owner_values
+//         )
 
-        // there might already be a where clause, but we dont want to lose that. So we combine the existing (if any)
-        // clause with the ownership clause
-        const combined_where = combine_wheres(
-            [value.where, ownership_where],
-            'AND'
-        )
+//         // there might already be a where clause, but we dont want to lose that. So we combine the existing (if any)
+//         // clause with the ownership clause
+//         const combined_where = combine_wheres(
+//             [value.where, ownership_where],
+//             'AND'
+//         )
 
-        // value could be true, since prisma allows nested selects like { goals: true } to select all columns on goals
-        return typeof value === 'object'
-            ? { ...value, where: combined_where }
-            : { where: combined_where }
-    })
+//         // value could be true, since prisma allows nested selects like { goals: true } to select all columns on goals
+//         return typeof value === 'object'
+//             ? { ...value, where: combined_where }
+//             : { where: combined_where }
+//     })
 
-    // prisma syntax treats the root table differently than all other tables (as in it is a method name, and not a property of
-    // 'select' or 'includes' like other nested tables), so we need to handle it separetely. But the logic is exactly the same.
-    const root_ownership_path = get_ownership_path(
-        root_ownership_paths,
-        root_table
-    )
-    const root_where = generate_ownership_where(
-        root_ownership_path,
-        owner_column,
-        owner_values
-    )
-    const combined_root_where = combine_wheres(
-        [root_where, prisma_query.where],
-        'AND'
-    )
+//     // prisma syntax treats the root table differently than all other tables (as in it is a method name, and not a property of
+//     // 'select' or 'includes' like other nested tables), so we need to handle it separetely. But the logic is exactly the same.
+//     const root_ownership_path = get_ownership_path(
+//         root_ownership_paths,
+//         root_table
+//     )
+//     const root_where = generate_ownership_where(
+//         root_ownership_path,
+//         owner_column,
+//         owner_values
+//     )
+//     const combined_root_where = combine_wheres(
+//         [root_where, prisma_query.where],
+//         'AND'
+//     )
 
-    return {
-        ...mapped_prisma_query,
-        where: combined_root_where,
-    }
-}
+//     return {
+//         ...mapped_prisma_query,
+//         where: combined_root_where,
+//     }
+// }
 
-/**
- * Gets the ownership path for a given table. Throws an error if it is not found (For security reasons, we need to ensure
- * no one forgot to make an ownership route)
- */
-export const get_ownership_path = (
-    ownership_paths: OwnershipPaths,
-    table_name: string
-) => {
-    if (ownership_paths[table_name] !== undefined) {
-        return ownership_paths[table_name]
-    }
+// /**
+//  * Gets the ownership path for a given table. Throws an error if it is not found (For security reasons, we need to ensure
+//  * no one forgot to make an ownership route)
+//  */
+// export const get_ownership_path = (
+//     ownership_paths: ConnectionPaths,
+//     table_name: string
+// ) => {
+//     if (ownership_paths[table_name] !== undefined) {
+//         return ownership_paths[table_name]
+//     }
 
-    throw new Error(`Could not find ownership path for table ${table_name}.`)
-}
+//     throw new Error(`Could not find ownership path for table ${table_name}.`)
+// }
 
-/**
- * Takes a list of where clauses and combines them with a connective. Handles cases where there are none,
- * one, or multiple where clauses
- */
-export const combine_wheres = (wheres: any[], connective: 'AND' | 'OR') => {
-    const valid_wheres = wheres.filter(el => !!el)
+// /**
+//  * Takes a list of where clauses and combines them with a connective. Handles cases where there are none,
+//  * one, or multiple where clauses
+//  */
+// export const combine_wheres = (wheres: any[], connective: 'AND' | 'OR') => {
+//     const valid_wheres = wheres.filter(el => !!el)
 
-    const combined_where =
-        valid_wheres.length === 0
-            ? undefined
-            : valid_wheres.length === 1
-            ? valid_wheres[0]
-            : { [connective]: valid_wheres }
+//     const combined_where =
+//         valid_wheres.length === 0
+//             ? undefined
+//             : valid_wheres.length === 1
+//             ? valid_wheres[0]
+//             : { [connective]: valid_wheres }
 
-    return combined_where
-}
+//     return combined_where
+// }
 
-/**
- * Returns true if the table_name is a key of the ownership_paths
- */
-export const is_table_name = (
-    table_name: string | number,
-    ownership_paths: OwnershipPaths
-) => {
-    return Object.keys(ownership_paths).includes(table_name?.toString())
-}
+// /**
+//  * Returns true if the table_name is a key of the ownership_paths
+//  */
+// export const is_table_name = (
+//     table_name: string | number,
+//     ownership_paths: ConnectionPaths
+// ) => {
+//     return Object.keys(ownership_paths).includes(table_name?.toString())
+// }
 
-/**
- * Generates a single ownership where clause
- */
-const generate_ownership_where = (
-    ownership_path: string[],
-    owner_column: string,
-    owner_values: any[]
-) => {
-    const inner_where = {
-        [owner_column]: {
-            in: owner_values,
-        },
-    } as Record<string, unknown>
+// /**
+//  * Generates a single ownership where clause
+//  */
+// const generate_ownership_where = (
+//     ownership_path: string[],
+//     owner_column: string,
+//     owner_values: any[]
+// ) => {
+//     const inner_where = {
+//         [owner_column]: {
+//             in: owner_values,
+//         },
+//     } as Record<string, unknown>
 
-    // we will build the where clause from the bottom up, starting with the end of the ownership path and working to the beginning.
-    // the .slice() makes a copy so that .reverse() doesnt mutate the original
-    const ownership_where = ownership_path
-        .slice()
-        .reverse()
-        .reduce((where, table_name) => {
-            return {
-                [table_name]: {
-                    is: where,
-                },
-            }
-        }, inner_where)
+//     // we will build the where clause from the bottom up, starting with the end of the ownership path and working to the beginning.
+//     // the .slice() makes a copy so that .reverse() doesnt mutate the original
+//     const ownership_where = ownership_path
+//         .slice()
+//         .reverse()
+//         .reduce((where, table_name) => {
+//             return {
+//                 [table_name]: {
+//                     is: where,
+//                 },
+//             }
+//         }, inner_where)
 
-    return ownership_where
-}
+//     return ownership_where
+// }
