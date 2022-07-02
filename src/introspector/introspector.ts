cd0b9f42d7c28b7c241c97eed59067e071c461db
Mendel <mendeljacks@gmail.com>
2022-07-02 04:53:30 -0400

    pg compat improvements

--- src/introspector/introspector.ts
+++ src/introspector/introspector.ts
@@ -17,6 +17,7 @@ export interface mysql_column {
     ordinal_position: number
     column_default?: string | number
     is_nullable?: string
+    is_identity?: 'YES' | 'NO' //postgres
     data_type: string
     character_maximum_length?: number
     numeric_precision?: number
@@ -30,7 +31,7 @@ export interface mysql_column {
 
 export interface mysql_index {
     table_name: string
-    non_unique: number | 'NO' | 'YES'
+    non_unique: number | '0'
     index_name: string
     seq_in_index: number
     column_name: string
@@ -67,7 +68,7 @@ export interface orma_entity_schema {
 
 export interface orma_field_schema {
     data_type?: keyof typeof mysql_to_typescript_types
-    character_count?: number | string
+    character_count?: number | string // string in postgres
     ordinal_position?: number
     decimal_places?: number
     not_null?: boolean
@@ -107,30 +108,58 @@ export const get_introspect_sqls = (
     db_type: SupportedDbs
 ): string[] => {
     /* selects: table_name, table_comment FROM INFORMATION_SCHEMA.TABLES column_name, table_name, data_type, column_type, column_key, is_nullable, numeric_precision, numeric_scale, character_maximum_length, column_default, extra, column_comment FROM information_schema.COLUMNS  table_name, column_name, referenced_table_name, referenced_column_name, constraint_name FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE table_name, non_unique, index_name, seq_in_index, column_name, collation, sub_part, packed, nullable, index_type, comment, index_comment, is_visible, expression FROM INFORMATION_SCHEMA.STATISTICS */
-    const query_strings = [
-        `SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='${database_name}'`,
-        `SELECT * FROM INFORMATION_SCHEMA.COLUMNS  WHERE table_schema = '${database_name}'`,
+
+    const tables = `SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='${database_name}'`
+    const columns =
+        db_type === 'postgres'
+            ? `SELECT * FROM INFORMATION_SCHEMA.COLUMNS  where table_schema = '${database_name}' and table_name in (
+        SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE table_schema='${database_name}'
+        )`
+            : `SELECT * FROM INFORMATION_SCHEMA.COLUMNS  WHERE table_schema = '${database_name}'`
+    const foreign_keys =
         db_type === 'postgres'
             ? `SELECT
-                    tc.table_schema, 
-                    tc.constraint_name, 
-                    tc.table_name, 
-                    kcu.column_name, 
-                    ccu.table_schema AS foreign_table_schema,
-                    ccu.table_name AS referenced_table_name,
-                    ccu.column_name AS referenced_column_name 
-                FROM 
-                    information_schema.table_constraints AS tc 
-                    JOIN information_schema.key_column_usage AS kcu
-                    ON tc.constraint_name = kcu.constraint_name
-                    AND tc.table_schema = kcu.table_schema
-                    JOIN information_schema.constraint_column_usage AS ccu
-                    ON ccu.constraint_name = tc.constraint_name
-                    AND ccu.table_schema = tc.table_schema
-                WHERE tc.constraint_type = 'FOREIGN KEY' and tc.table_schema = '${database_name}'`
-            : `SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_SCHEMA = '${database_name}'`,
-        `SELECT * FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = '${database_name}'`,
-    ]
+            tc.table_schema, 
+            tc.constraint_name, 
+            tc.table_name, 
+            kcu.column_name, 
+            ccu.table_schema AS foreign_table_schema,
+            ccu.table_name AS referenced_table_name,
+            ccu.column_name AS referenced_column_name 
+        FROM 
+            information_schema.table_constraints AS tc 
+            JOIN information_schema.key_column_usage AS kcu
+            ON tc.constraint_name = kcu.constraint_name
+            AND tc.table_schema = kcu.table_schema
+            JOIN information_schema.constraint_column_usage AS ccu
+            ON ccu.constraint_name = tc.constraint_name
+            AND ccu.table_schema = tc.table_schema
+        WHERE tc.constraint_type = 'FOREIGN KEY' and tc.table_schema = '${database_name}'`
+            : `SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE REFERENCED_TABLE_SCHEMA = '${database_name}'`
+    const indexes =
+        db_type === 'postgres'
+            ? ` 
+            select 
+            pgc.conname as index_name,
+                   ccu.table_schema as table_schema,
+                   0 as non_unique,
+                   ccu.table_name,
+                   ccu.column_name,
+                   contype,
+                    pg_get_constraintdef(pgc.oid)
+            from pg_constraint pgc
+                     join pg_namespace nsp on nsp.oid = pgc.connamespace
+                     join pg_class  cls on pgc.conrelid = cls.oid
+                     left join information_schema.constraint_column_usage ccu
+                               on pgc.conname = ccu.constraint_name
+                                   and nsp.nspname = ccu.constraint_schema
+            where table_schema = '${database_name}'
+            and contype in ('p', 'u')
+            and table_name in (
+            SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE tables.table_schema='${database_name}'
+            )`
+            : `SELECT * FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_SCHEMA = '${database_name}'`
+    const query_strings = [tables, columns, foreign_keys, indexes]
 
     return query_strings
 }
@@ -257,6 +286,7 @@ export const generate_field_schema = (mysql_column: mysql_column) => {
         character_maximum_length,
         numeric_precision,
         numeric_scale,
+        is_identity,
         datetime_precision,
         column_key,
         extra,
@@ -279,7 +309,7 @@ export const generate_field_schema = (mysql_column: mysql_column) => {
         field_schema.indexed = true
     }
 
-    if (column_key === 'PRI') {
+    if (column_key === 'PRI' || is_identity === 'YES') {
         field_schema.primary_key = true
     }
 
@@ -333,10 +363,13 @@ export const generate_index_schemas = (mysql_indexes: mysql_index[]) => {
         const index_schemas = Object.keys(mysql_indexes_by_name).map(
             index_name => {
                 const index = mysql_indexes_by_name[index_name][0]
-                const fields = mysql_indexes_by_name[index_name]
-                    .slice()
-                    .sort((a, b) => a.seq_in_index - b.seq_in_index)
-                    .map(el => el.column_name)
+                const uniq = <T>(els: T[]): T[] => [...new Set(els)]
+                const fields = uniq(
+                    mysql_indexes_by_name[index_name]
+                        .slice()
+                        .sort((a, b) => a.seq_in_index - b.seq_in_index)
+                        .map(el => el.column_name)
+                )
                 return generate_index_schema(index, fields)
             }
         )
@@ -368,8 +401,7 @@ const generate_index_schema = (mysql_index: mysql_index, fields: string[]) => {
 
     const orma_index_schema: orma_index_schema = {
         index_name,
-        is_unique:
-            Number(non_unique) === 0 || non_unique === 'NO' ? true : false,
+        is_unique: Number(non_unique) === 0 ? true : false,
         fields,
         index_type,
         invisible: is_visible === 'NO',
