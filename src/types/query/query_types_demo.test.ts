5c64ba1cb5dd444c48420ac5d34a4b6483b48c25
Daniel Jacks <danielchaimjacks@gmail.com>
2023-05-29 11:26:34 -0400

    Fix query types demo

--- src/types/query/query_types_demo.test.ts
+++ src/types/query/query_types_demo.test.ts
@@ -1,11 +1,10 @@
-import { OrmaQueryResult, orma_query } from '../..'
+import { orma_query } from '../..'
 import {
     GlobalTestAliases,
     GlobalTestQuery,
     GlobalTestSchema,
     global_test_schema,
 } from '../../test_data/global_test_schema'
-import { QueryResultComplex } from './query_result_types'
 
 const t = () => {
     const query = {
@@ -19,24 +18,12 @@ const t = () => {
         },
     } as const satisfies GlobalTestQuery
 
-    // orma_query<GlobalTestSchema, GlobalTestAliases, typeof query>(
-    //     query,
-    //     global_test_schema,
-    //     async () => ({} as any)
-    // ).then(result => {
-    //     // result.my_posts[0].id
-    //     // result.my_posts[0].comments[0].id
-
-    //     const r: OrmaQueryResult<
-    //         GlobalTestSchema,
-    //         GlobalTestAliases,
-    //         typeof query
-    //     > = {}
-    // })
-
-    type T = QueryResultComplex<
-        GlobalTestSchema,
-        GlobalTestAliases,
-        typeof query
-    >
+    orma_query<GlobalTestSchema, GlobalTestAliases, typeof query>(
+        query,
+        global_test_schema,
+        async () => ({} as any)
+    ).then(result => {
+        result.my_posts?.[0].id
+        result.my_posts?.[0].comments?.[0].id
+    })
 }
